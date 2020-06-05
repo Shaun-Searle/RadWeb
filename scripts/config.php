b862b2abcaebc96e0552acbfd1c61b6d0b1cb425
Shaun Searle <M204225@tafe.wa.edu.au>
2020-06-05 11:44:07 +0800

    added config for sql connection

---
+++ scripts/config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * MySQLI Config
+ *
+ * PHP Version 7.4.3
+ *
+ * @category Config
+ * @package  MovieDB
+ * @author   Shaun Searle <M204225@tafe.wa.edu.au>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     No Link
+ */
+
+    const DB_HOST = "localhost";
+    const DB_NAME = "movie_db";
+    const DB_USER = "root";
+    const DB_PASS = "";
+
+
+?>
